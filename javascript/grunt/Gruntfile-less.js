1dcfa1db7fb640e2334ba6edee8cc6561c540be0
Victor Chavarro <info@vianch.com>
2014-11-07 09:57:47 -0500

    Gruntfile-less
    Gruntfile-less
    with watcher and cssmin,
    the minifaction can be used un less too
    with compress: true

---
+++ javascript/grunt/Gruntfile-less.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+
+         credits: grunt.template.process("/**\n * Author: <%= name %>\n * Date: <%= grunt.template.today('yyyy-mm-dd') %> \n **/\n",{data:{name:"Victor Chavarro"}}),
+
+        /*LESS COMPILER*/
+        less: {
+            development: {
+                options: {
+                    paths: ["assets/css"],
+                    cleancss: true
+                   /* compress: true*/
+                },
+                files: {
+                    "ts/app/ui/css/result.css": "ts/app/ui/css/style.less"
+                }
+            },
+            production: {
+                options: {
+                    paths: ["assets/css"],
+                    cleancss: true,
+                    modifyVars: {
+                        imgPath: '"http://mycdn.com/path/to/images"',
+                        bgColor: 'red'
+                    }
+                },
+                files: {
+                    "path/to/result.css": "path/to/source.less"
+                }
+            }
+        },
+
+        cssmin:{
+            options:{
+                banner: "<%= credits %>"
+            },
+
+            production:{
+                files: [{
+                    expand: true,
+                    cwd: 'ts/app/ui/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'ts/app/ui/css/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        /*LIVE COMPILERS*/
+        watch:{
+            less:{
+                files: "ts/app/ui/css/*.less",
+                tasks: ['less:development']
+            }
+        }
+    });
+
+	/*LIBRERIES*/
+    grunt.loadNpmTasks('grunt-contrib-watch');  //watch changes and run task automatically
+    grunt.loadNpmTasks('grunt-contrib-sass'); // SASS compiler
+
+
+};
